4bcb8ca494c7fb44c8f1f7dd4c7fdf248969b51b
yashrajyadav2002 <yyash9631@gmail.com>
2025-04-02 18:33:54 +0530

    02april

---
+++ newfold.cpp/datatype.cpp
@@ -0,0 +1,153 @@
+#include<iostream>
+using namespace std;
+
+//interger type ?
+// int main(){
+
+//     int num;
+//     num=15;
+//     cout<<num;
+//       return 0;
+// }
+//  //charactor type ?
+// int main(){
+//     char a='b';
+
+//     cout<<a;
+//     return 0;
+// }
+
+// // float type ?
+// int main(){
+//     float salary;
+//     salary=25000;
+//     cout<<salary;
+//     return 0;
+
+// }
+
+// //double type ?
+
+// int main(){
+//     double price=25.6;
+//     cout<<price;
+//     return 0;
+
+// }
+
+// // find the area of the circle ?
+
+// int main(){
+
+//     float area , radious;
+
+//     cout<<"enter the radious";
+//     cin>>radious;
+//     area=3.14*radious*radious;
+
+//     cout<<"area of the circle is"<<area;
+//     return 0;
+
+// }
+
+// //find the area of the triange ?
+
+// int main(){
+//     float area,base ,height;
+
+//     cout<<"enter the base ";
+//     cin>>base;
+//     cout<<"enter the height";
+//     cin>>height;
+
+//     area=base*height/2;
+
+//     cout<<"area of the triangle is"<<area;
+//     return 0;
+
+// }
+
+// //find the area of rectangle ?
+
+// int main(){
+//     float area, length , width;
+
+//     cout<<"enter the length of rectangle";
+//     cin>>length;
+
+//     cout<<"enter the width of rectangle";
+//     cin>>width;
+
+//     area=length*width;
+
+//     cout<<"the area of rectangle is"<<area;
+
+//     return 0;
+// }
+
+//earthmatic operator?
+
+// int main(){
+//     int a=20,b=30;
+//     cout<<a+b<<endl;
+//     cout<<a-b<<endl;
+//     cout<<a*b<<endl;
+//     cout<<a/b<<endl;
+//     return 0;
+// }
+
+
+//logical operator?
+
+// int main(){
+//     int a,b;
+//     cout<<"enter the num";
+//     cin>>a>>b;
+
+//     (a%2==0&&b%2==0)?cout<<"this is div.."<<endl:cout<<"not div..";
+//     return 0;
+    
+// }
+
+// int main(){
+//    int year;
+//    cout<< " enter year ";
+//    cin>> year;
+
+//    (year%400==0 || year%4==0)? cout<< " its a leap year ": cout<< " normal year" ;
+//    return 0;
+// }
+
+
+//find the frater?
+
+// int main(){
+//     int a;
+//     cout<<"enter the num";
+//     cin>>a;
+
+//     (a>=10&&a<=50)?cout<<"the number is between 10 or 50":cout<<"the number not between 10 or 50 ";
+//     return 0;
+// }
+
+
+//using ternery operator?
+
+int main(){
+    int a;
+    cout<<"enter pass.";
+    cin>>a;
+
+    (a==1234)?cout<<"welcome":(cout<<"incorrect"<<endl<<"enter pass again",cin>>a)
+
+}
+
+
+
+
+
+
+
+
+
+
